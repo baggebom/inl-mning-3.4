8a5eaa5f37d1f578e787d1538bec27010b2852f5
Admin <Admin@TEB123004>
2023-10-20 14:54:59 +0200

    Add project files.

---
+++ inlämning 3.4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Upggift
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej, skriv in hur lång din låt är för våran radiostation i minuter");
+            int låtM = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många sekunder är din låt");
+            int låtS = int.Parse(Console.ReadLine());
+
+            int låtL = (låtM * 60) + låtS;
+
+
+            if (låtM < 260)
+            {
+                Console.WriteLine("ja denna kan vi spela");
+            }
+            else
+            {
+                Console.WriteLine("Tyvärr denna är för lång");
+            }
+            
+
+        }
+
+    }
+}
